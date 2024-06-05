e344aa800ba8b6eb34d672024df6eff556109824
linhtql <tqlinh.na@gmail.com>
2024-06-05 09:34:20 +0700

    Init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoText, setTodoText] = useState('');
+
+  const addTodo = () => {
+    if (todoText.trim()) {
+      setTodos([...todos, { id: Date.now(), text: todoText }]);
+      setTodoText('');
+    }
+  };
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="title">Todo List</h1>
+        <div className="input-container">
+          <input
+            type="text"
+            value={todoText}
+            onChange={e => setTodoText(e.target.value)}
+            placeholder="Enter a new todo..."
+            className="input"
+          />
+          <button onClick={addTodo} className="add-button">Add Todo</button>
+        </div>
+        <ul className="todo-list">
+          {todos.map(todo => (
+            <li key={todo.id} className="todo-item">
+              {todo.text}
+              <button onClick={() => deleteTodo(todo.id)} className="delete-button">Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
